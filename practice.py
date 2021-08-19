b30e6abc850894212fa0ab500741d10e5c724e20
Mita-cpu <82151864+Mita-cpu@users.noreply.github.com>
2021-08-19 16:42:07 +0200

    Added content

---
+++ practice.py
@@ -0,0 +1,70 @@
+#Python Iteration activities
+
+#  A method that takes in a string word and returns the true if the word is a palindrome, false otherwise.
+def is_palindrome(word):
+    characters = list(word)
+    is_palindrome = True
+
+    for character in characters:
+        if character[0] == characters[-1]:
+            characters.pop()
+        else:
+            is_palindrome= False
+            break
+
+    print(is_palindrome)
+
+is_palindrome("took")
+
+
+#  A method that takes an array of numbers and returns a new array ,
+# where every element of the original array is multiplied by 2.
+
+def doubler(numbers):
+    my_new_array = []
+    for i in numbers:
+        my_new_array.append(i * 2)
+
+    print(my_new_array)
+
+doubler([3,4,2,5])
+
+
+# A method that takes in a string name and returns a string saying bye to that name.
+
+def goodbye(name):
+
+    print("Good bye",name)
+
+goodbye("Mita")
+
+
+# A method that returns the average of three numbers
+
+def average_of_three(num1, num2, num3):
+    total_average= (num1 + num2 + num3) // 3
+
+    print(total_average)
+
+average_of_three(20,43,2)
+
+
+# A method that takes in a number and returns an array containing all numbers greater than 0 and 
+# less the number that are divisible by either 4 or 6, but not both.
+
+def fizz_buzz(max):
+    nums = []
+    i = 0
+    while i < max:
+        
+        if (i % 4 == 0 or i % 6 == 0) and not(i % 4 == 0 and i % 6 == 0):
+            nums.append(i)
+        i += 1 
+
+    print(nums)
+
+fizz_buzz(100)
+
+
+###### END    
+
